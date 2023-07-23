c6dd3ed69c578331a6b8b0ebdcbe270520c864f3
Mr.Aidin <aidinsotudeh@gmail.com>
2023-07-23 17:53:17 +0330

    first commit

---
+++ vue-breeze-api/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+        <Nav></Nav>
+        <RouterView></RouterView>
+    </main>
+</template>
+<script setup>
+
+import Nav from './components/Nav.vue'
+
+
+</script>
---
+++ vue-breeze-api/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="authStore.user">
+        <h1>{{authStore.user.name}}</h1>
+        <h2>{{authStore.user.email}}</h2>
+    </div>
+    <div v-else>
+        Go To login
+    </div>
+    <h1>{{ user?.name }}</h1>
+    <p>{{ user?.email }}</p>
+</template>
+
+<script setup>
+import {onMounted} from "vue";
+import {useAuthStore} from "../stores/auth.js";
+
+const authStore = useAuthStore();
+onMounted(async () => {
+    await authStore.getUser()
+
+});
+
+</script>
+
+
