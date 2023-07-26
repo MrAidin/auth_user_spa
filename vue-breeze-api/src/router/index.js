9b66a35db56745e1282fda2a050d099ee0d501c8
Mr.Aidin <aidinsotudeh@gmail.com>
2023-07-26 17:36:53 +0330

    final Commit

--- vue-breeze-api/src/router/index.js
+++ vue-breeze-api/src/router/index.js
@@ -6,17 +6,59 @@ const routes = [
     {
         path: '/',
         name:'Home',
-        component: Home
+        component: Home,
+        meta: {
+            progress: {
+                func: [
+                    { call: "color", modifier: "temp", argument: "#ffb000" },
+                    { call: "fail", modifier: "temp", argument: "#6e0000" },
+                    { call: "location", modifier: "temp", argument: "top" },
+                    {
+                        call: "transition",
+                        modifier: "temp",
+                        argument: { speed: "1.5s", opacity: "0.6s", termination: 400 },
+                    },
+                ],
+            },
+        },
     },
     {
         path: '/login',
         name:'Login',
-        component: () => import('../components/Login.vue')
+        component: () => import('../components/Login.vue'),
+        meta: {
+            progress: {
+                func: [
+                    { call: "color", modifier: "temp", argument: "#ffb000" },
+                    { call: "fail", modifier: "temp", argument: "#6e0000" },
+                    { call: "location", modifier: "temp", argument: "top" },
+                    {
+                        call: "transition",
+                        modifier: "temp",
+                        argument: { speed: "1.5s", opacity: "0.6s", termination: 400 },
+                    },
+                ],
+            },
+        },
     },
     {
         path: '/register',
         name:'Register',
-        component: () => import('../components/Register.vue')
+        component: () => import('../components/Register.vue'),
+        meta: {
+            progress: {
+                func: [
+                    { call: "color", modifier: "temp", argument: "#ffb000" },
+                    { call: "fail", modifier: "temp", argument: "#6e0000" },
+                    { call: "location", modifier: "temp", argument: "top" },
+                    {
+                        call: "transition",
+                        modifier: "temp",
+                        argument: { speed: "1.5s", opacity: "0.6s", termination: 400 },
+                    },
+                ],
+            },
+        },
     },
 
 ];
